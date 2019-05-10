6ffee3fa54709eebae7d28179dcc6b2e2883e4b3
ChueiZuoChen <louisa0103a@gmail.com>
2019-05-11 01:22:08 +0800

    QuerySnapshot process

---
+++ app/src/main/java/com/paul/shop/FirestoreQueryLiveData.kt
@@ -0,0 +1,32 @@
+package com.paul.shop
+
+import androidx.lifecycle.LiveData
+import com.google.firebase.firestore.*
+
+class FirestoreQueryLiveData : LiveData<QuerySnapshot>(), EventListener<QuerySnapshot> {
+    lateinit var registration:ListenerRegistration
+    var query = FirebaseFirestore.getInstance()
+        .collection("items")
+        .orderBy("viewcount", Query.Direction.DESCENDING)
+        .limit(10)
+
+    var isRegistered = false
+
+    override fun onActive() {
+        registration = query.addSnapshotListener(this)
+        isRegistered = true
+    }
+
+    override fun onInactive() {
+        super.onInactive()
+        if (isRegistered) {
+            registration.remove()
+        }
+    }
+
+    override fun onEvent(querySnapshot: QuerySnapshot?, exception: FirebaseFirestoreException?) {
+        if (querySnapshot!= null && !querySnapshot.isEmpty) {
+            value = querySnapshot
+        }
+    }
+}
--- app/src/main/java/com/paul/shop/ItemViewModel.kt
+++ app/src/main/java/com/paul/shop/ItemViewModel.kt
@@ -1,29 +1,13 @@
 package com.paul.shop
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.Query
 
 class ItemViewModel: ViewModel() {
     private var items = MutableLiveData<List<Item>>()
-    fun getItems():MutableLiveData<List<Item>>{
-        FirebaseFirestore.getInstance()
-            .collection("items")
-            .orderBy("viewcount",Query.Direction.DESCENDING)
-            .limit(10)
-            .addSnapshotListener { querySnapshot, firebaseFirestoreException ->
-                if (querySnapshot !=null && !querySnapshot.isEmpty){
-                    var list = mutableListOf<Item>()
-                    for (doc in querySnapshot) {
-                        val item = doc.toObject(Item::class.java)
-                        item.id = doc.id
-                        list.add(item)
-                    }
-                    items.value = list
-//                    items.value = querySnapshot.toObjects(Item::class.java)
-                }
-            }
-        return items
+    private var firestoreQueryLiveData = FirestoreQueryLiveData()
+    fun getItems():FirestoreQueryLiveData{
+        return firestoreQueryLiveData
     }
 }
